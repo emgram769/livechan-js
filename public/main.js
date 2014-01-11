a68aacbd66fe99b4b05f59885a2e50cc292290bb
Bram <bwasti@gmail.com>
2014-01-11 05:28:55 -0500

    UPDATE

--- public/main.js
+++ public/main.js
@@ -1,12 +1,19 @@
 var socket = io.connect('/');
 var chat = [];
 
+function escapeHTML( string )
+{
+    var pre = document.createElement('pre');
+    var text = document.createTextNode( string );
+    pre.appendChild(text);
+    return pre.innerHTML;
+}
+
 function draw_new_chat(data){
-    var name = "<span class='chat_name'>"+data.name+"</span>"+data.date+"<br/>"
+    var name = "<span class='chat_name'>"+escapeHTML(data.name)+"</span>"+data.date+"<br/>"
     var new_image = data.image ? "<img height='100px' class='chat_img' src='/"+data.image.slice(7)+"' onClick='window.location.href=\"/"+data.image.slice(7)+"\"'>" : "";
-    var new_chat = "<div class='chat'>"+name+new_image+data.body+"</div>";
-    $('.chats:first').append(new_chat);  
-    
+    var new_chat = "<div class='chat'>"+name+new_image+escapeHTML(data.body).replace(/\r?\n/g, '<br />')+"</div>";
+    $('.chats:first').append(new_chat);
 }
 
 function scroll(clear){
