c8cc12438c2eaac1f8820e8da165ea3bcba287c3
emoose <abc@cock.li>
2014-01-14 15:29:49 +0000

    cleanup thumbhover

--- public/jquery.thumbhover.js
+++ public/jquery.thumbhover.js
@@ -83,14 +83,6 @@
 			$(this).data("hovered", false);
 			$(popup).empty().hide();
 		}
-		function getDocHeight() {
-			var D = document;
-			return Math.max(
-				D.body.scrollHeight, D.documentElement.scrollHeight,
-				D.body.offsetHeight, D.documentElement.offsetHeight,
-				D.body.clientHeight, D.documentElement.clientHeight
-			);
-		}
 		function getWindowSize() {
 			return {
 				scrollLeft: $(window).scrollLeft(),
